278e09d7b61b6cfdbd37d62e0e7caca51ccdedeb
flemerrer <francois.lemerrer@pm.me>
2023-09-11 10:01:45 +0200

    mapped entities

---
+++ src/main/java/com/example/ecommerce/entities/Boardgame.java
@@ -0,0 +1,102 @@
+package com.example.ecommerce.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="BOARDGAMES")
+public class Boardgame {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="GAME_ID")
+    private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CATEGORY_ID")
+    private Category category;
+
+    String name;
+    String publisher;
+    int score;
+    //stocker en centimes
+    int price;
+    String description;
+
+    public Boardgame(String name, String publisher, int score, int price, String description) {
+        this.name = name;
+        this.publisher = publisher;
+        this.score = score;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Boardgame{" +
+                "id=" + id +
+                ", category=" + category +
+                ", name='" + name + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", score=" + score +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/ecommerce/entities/Category.java
@@ -0,0 +1,48 @@
+package com.example.ecommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name="CATEGORY")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "GAME_ID")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "category")
+    List<Boardgame> boardgameList;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Boardgame> getBoardgameList() {
+        return boardgameList;
+    }
+
+    public void setBoardgameList(List<Boardgame> boardgameList) {
+        this.boardgameList = boardgameList;
+    }
+}
